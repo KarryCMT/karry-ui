573de1ac1ca33c3e86233df23b7f6b2451d5db04
litao <1570606465@qq.com>
2021-11-30 13:49:56 +0800

    fix:优化demo样式

--- examples/main.js
+++ examples/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '../components/css/demo.scss';
-import Demo from '../components/lib/demo/index';
+import '../components/css/index.scss';
+import Demo from '../components/lib/button/index';
 Vue.use(Demo);
 Vue.config.productionTip = false
 
